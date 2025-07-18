5f0e67117f7bb0aa482d996c3510866340d073c5
Mohommed-Iflan <mohommediflaan@gmail.com>
2025-07-18 15:20:31 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ async def handle_link(message: types.Message):
     # Expect format: filename.mp4 https://t.me/...
     parts = text.split()
     if len(parts) != 2:
-        await message.answer("❌ Format:\n<filename>.mp4 <telegram_file_link>")
+        await message.answer("❌ Format:\n&lt;filename&gt;.mp4 &lt;telegram_file_link&gt;")
         return
 
     filename, file_link = parts
